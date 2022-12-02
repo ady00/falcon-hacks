7b7231e74820e6bb78332cc8ada7f994ccba87b7
Ady <advayb2018@gmail.com>
2022-12-01 23:14:06 -0800

    reformatted main page into falcon-hacks only

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,35 @@
 import type { NextPage } from "next";
-import { Calendar } from "../components/Calendar";
-import DotMatrix from "../components/dotMatrix";
-import { Events } from "../components/events";
-import Officers from "../components/Officers";
-import { Square } from "../components/square";
+import { Calendar } from "../components/countdown";
+import DotMatrix from "../components/homePage";
+import Officers from "../components/prizes";
+import { Square } from "../components/rectangle";
+import { Schedule } from "../components/schedulehacks";
+import { Faq } from "../components/faq";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    document.body.classList.remove("hid");
+  }, []);
   return (
-    <div className="grid grid-cols-1 p-4 gap-4 sm:grid-cols-4">
+    <div className="p-4 gap-4">
       <DotMatrix />
-      <Square span="col-span-2" />
-      <Calendar />
-      <Officers />
-      <Events />
+
+      <div className="py-2"></div>
+
+      <div>
+        <Square span="col-span-2" />
+        <div className="py-2"></div>
+      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-4">
+        <Schedule />
+        <Officers />
+        <Calendar />
+      </div>
+      <div className="py-4">
+        <Faq span="col-span-2" />
+        <div className="py-2"></div>
+      </div>
     </div>
   );
 };
