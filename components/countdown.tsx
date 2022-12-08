1c93f1def7a9f41454b3a6d8f801e51fdb23218a
arnavg115 <soccercream20@gmail.com>
2022-12-08 09:19:13 +0000

    done

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -7,18 +7,16 @@ export const Calendar = () => {
 
   return (
     <div
-      className={`w-full border-gray-300 border-4 rounded-lg font-dot text-white p-2 justify-between col-span-2 sm:col-span-1 box`}
+      className={`w-full border-gray-300 border-4 rounded-lg font-dot text-white p-2 justify-between col-span-1 box`}
     >
       {/* <div> */}
-      <div className="w-full mb-10">
+      <div className="w-full h-full">
         <h1 className="text-3xl font-dot">Countdown</h1>
         <Divider color="bg-slate-500" />
 
-        <div className="w-full flex flex-col items-center disp">
-          <h1 style={{ fontSize: "15vw", lineHeight: 0.8 }}>
-            {date}
-          </h1>
-          <p className = "py-4"> </p>
+        <div className="w-full flex flex-col items-center disp justify-center h-3/4">
+          <h1 style={{ fontSize: "15vw", lineHeight: 0.8 }}>{date}</h1>
+          <p className="py-4"> </p>
           <p className="text-3xl">Days Left</p>
         </div>
         {/* </div> */}
